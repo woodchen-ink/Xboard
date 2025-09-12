451dcd97be99b9ebc51ede65454043a866036c91
wood chen <wood@czl.net>
2025-09-12 17:06:52 +0800

    feat(theme): 添加CZLPanel主题并移除Xboard主题
    - 新增CZLPanel主题相关文件，包括favicon、资源文件、配置文件等
    - 删除原有的Xboard主题及其所有相关文件
    - 更新.gitignore文件以简化主题管理
    - 添加CZLPanel主题的dashboard页面和配置选项

--- theme/CZLPanel/dashboard.blade.php
+++ theme/CZLPanel/dashboard.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no" />
   <title>{{$title}}</title>
-  <script type="module" crossorigin src="/theme/{{$theme}}/assets/umi.js"></script>
+  <script type="module" crossorigin src="/theme/{{$theme}}/assets/czlpanel.js"></script>
 </head>
 
 <body>
